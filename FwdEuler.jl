f0fa532ced94b2a5910f68426ab4ed90f6efb099
Norbert Szulc <norbert@not7cd.net>
2019-01-07 11:30:08 +0100

    Rename, tweaks

--- FwdEuler.jl
+++ FwdEuler.jl
@@ -5,17 +5,15 @@ gr()
 theme(:solarized_light)
 
 dt = 0.00001
+m1 = 0.6
+m2 = 1 - m1
 
 mutable struct Particle
     r::Vector{Float64}
     v::Vector{Float64}
 end
 
-
-p1 = Particle([1., 0., 0.], [0, .5, 0])
-
 function step!(p::Particle)
-    # len_r2 =
     len_r2 = dot(p.r, p.r)
     len_r3 = len_r2 * sqrt(len_r2)
     a = - (p.r / len_r3);
@@ -23,21 +21,15 @@ function step!(p::Particle)
     p.v += a .* dt
 end
 
+p1 = Particle([1., 0., 0.], [0, .5, 0])
+
 # initialize a 3D plot with 1 empty series
-err_plt = plot(2, title= "Error")
-plt = plot(1,
-                title = "PJP <3", marker = 1, size = (800, 800),
-                m = (:cross, 2, stroke(0)))
+err_plt = plot(1, title= "Error")
+plt = plot(1, xlim=(-1.2,1.2), ylim=(-1.2,1.2),
+                title = "PJP <3", size = (800, 800),
+                m = (:cross, 3, stroke(0)))
 
-# err1 = dot(p1.r, p1.r)
-# println(p1, err1)
-# step!(p1)
-# err2 = dot(p1.r, p1.r)
-# println(p1, err2)
-
-# println("Error ", err2/err1 - 1, "%")
-
-n = 100
+n = 300
 prog = Progress(n,1)
 # build an animated gif by pushing new points to the plot, saving every 10th frame
 for i=1:n
